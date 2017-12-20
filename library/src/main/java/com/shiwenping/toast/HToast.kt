2063c0cadb989b2bf7c5a68db44d01a8158b4d9d
石文平 <hqkl@vip.qq.com>
2017-12-20 17:09:09 +0800

    1.修改kotlin

--- library/src/main/java/com/shiwenping/toast/HToast.kt
+++ library/src/main/java/com/shiwenping/toast/HToast.kt
@@ -43,6 +43,10 @@ class HToast(context: Context?, toastStyle: ToastStyle) {
         return this
     }
 
+    fun isShowing(): Boolean {
+        return toastDialog!!.getDialog().isShowing
+    }
+
     /**
      * 隐藏
      */
